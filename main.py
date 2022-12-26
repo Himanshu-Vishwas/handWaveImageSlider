0c57f5478943d161a6969faef264599eeb1077f5
Himanshu-vishwas <himanshuvishwas58@gmail.com>
2022-12-26 16:52:22 +0530

    Changes to be committed: 	new file:   main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+import cv2 as cv
+import pyautogui as p
+import time as t
+from cvzone.HandTrackingModule import HandDetector
+
+
+detector = HandDetector(detectionCon=0.8, maxHands=1)
+
+video = cv.VideoCapture(0)
+history = ["himanshu",int(t.time())]
+
+def final(loc,tdif):
+    if loc != history[0]:
+                if tdif <=0.04 and tdif>=0.0003:
+                    if history[0] != "himanshu":
+                        print(history[0]," to ",loc)
+                        p.press(loc)
+                    history[0] = loc
+                    history[1] = tdif
+
+
+while True:
+    ret,frame = video.read()
+    hands,img = detector.findHands(frame)
+    cv.imshow("Frame",frame)
+
+
+    if hands:
+        hand1 = hands[0]
+        xcor = hand1["center"][0]
+        t_his = t.time()/0.01
+
+        history[1] = t_his
+        
+        if xcor<=250:
+            pos = "left"
+            t_dif = (t.time()/0.01)- t_his
+            final(pos,t_dif)
+
+
+        elif xcor>250:
+            pos="right"
+            t_dif = (t.time()/0.01)- t_his
+            final(pos,t_dif)
+            
+            
+        else:
+            continue
+    key = cv.waitKey(1)
+    if not hands:
+        history[0] = "himanshu"
+
+    if key == ord('x'):
+        break
+
+video.release()
+cv.destroyAllWindows()
